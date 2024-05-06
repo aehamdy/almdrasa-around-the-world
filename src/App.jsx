df9de849246005936a4a5f1c3221f66ffe87f1be
aehamdy <ae.hamdy91@gmail.com>
2024-05-06 18:28:56 +0300

    fix: some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Country from "./pages/Country";
 import NoPage from "./pages/NoPage";
-import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route index element={<Home />} />
-        <Route path=":country" element={<Country />} />
-        <Route path="*" element={<NoPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path=":country" element={<Country />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
